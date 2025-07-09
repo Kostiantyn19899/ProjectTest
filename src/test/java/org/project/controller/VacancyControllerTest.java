b69b80e8deff464aee8972db10f6af76753db3ba
Kostiantyn19899 <sierikov.de@gmail.cim>
2025-07-09 07:54:20 +0200

    fix test add @ActiveProfiles("test")

--- src/test/java/org/project/controller/VacancyControllerTest.java
+++ src/test/java/org/project/controller/VacancyControllerTest.java
@@ -16,6 +16,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.Arrays;
@@ -27,6 +28,7 @@ import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+@ActiveProfiles("test")
 @WebMvcTest(VacancyController.class)
 class VacancyControllerTest {
 
